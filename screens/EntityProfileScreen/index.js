c434826e6cabbd50f2adde912fb1b7057398a850
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-11-14 02:05:22 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/EntityProfileScreen/index.js
+++ screens/EntityProfileScreen/index.js
@@ -14,22 +14,22 @@ const ScreenComponent = () => {
     totalUsers: 100,
     users: ['User1', 'User2', 'User3']
   };
-  return <SafeAreaView style={_styles.kHOprLNc}>
-      <ScrollView style={_styles.JYkMVsJR}>
-        <Text style={_styles.UkORzbYT}>{entity.title}</Text>
+  return <SafeAreaView style={_styles.NNIiAHhO}>
+      <ScrollView style={_styles.nOlYhPEu}>
+        <Text style={_styles.lYNVAuGc}>{entity.title}</Text>
         <Image source={{
         uri: entity.imageUrl
-      }} style={_styles.wXPRkisY} />
-        <Text style={_styles.MsnzhBjh}>{entity.description}</Text>
-        <Text style={_styles.fQAJqQpr}>Contact Info: {entity.contactInfo}</Text>
-        <Text style={_styles.hUgkayJv}>Location Info: {entity.locationInfo}</Text>
-        <Text style={_styles.jJYmLSxz}>Other Details: {entity.otherDetails}</Text>
-        <Text style={_styles.biaJOHwA}>Total Users: {entity.totalUsers}</Text>
-        <Text style={_styles.MtTywSSj}>Users: {entity.users.join(', ')}</Text>
+      }} style={_styles.uKvPZpFq} />
+        <Text style={_styles.cTHZzerS}>{entity.description}</Text>
+        <Text style={_styles.fHPvGzDy}>Contact Info: {entity.contactInfo}</Text>
+        <Text style={_styles.WEOsbeiK}>Location Info: {entity.locationInfo}</Text>
+        <Text style={_styles.RbYqzGub}>Other Details: {entity.otherDetails}</Text>
+        <Text style={_styles.UhSbihUc}>Total Users: {entity.totalUsers}</Text>
+        <Text style={_styles.iZCwRDzp}>Users: {entity.users.join(', ')}</Text>
         <Button title="Report Entity" onPress={() => alert('Report Entity')} />
-        <Text style={_styles.GyezklFA}>
+        <Text style={_styles.hXHwWiWV}>
           Website: 
-          <Text style={_styles.AnUdFtbf} onPress={() => Linking.openURL(entity.websiteLink)}>
+          <Text style={_styles.ecaMnSZn} onPress={() => Linking.openURL(entity.websiteLink)}>
             {entity.websiteLink}
           </Text>
         </Text>
@@ -40,51 +40,51 @@ const ScreenComponent = () => {
 export default ScreenComponent;
 
 const _styles = StyleSheet.create({
-  kHOprLNc: {
+  NNIiAHhO: {
     flex: 1,
     backgroundColor: "#fff"
   },
-  JYkMVsJR: {
+  nOlYhPEu: {
     padding: 20
   },
-  UkORzbYT: {
+  lYNVAuGc: {
     fontSize: 24,
     fontWeight: "bold"
   },
-  wXPRkisY: {
+  uKvPZpFq: {
     width: "100%",
     height: 200,
     resizeMode: "cover",
     marginVertical: 20
   },
-  MsnzhBjh: {
+  cTHZzerS: {
     fontSize: 18
   },
-  fQAJqQpr: {
+  fHPvGzDy: {
     fontSize: 18,
     marginTop: 20
   },
-  hUgkayJv: {
+  WEOsbeiK: {
     fontSize: 18,
     marginTop: 20
   },
-  jJYmLSxz: {
+  RbYqzGub: {
     fontSize: 18,
     marginTop: 20
   },
-  biaJOHwA: {
+  UhSbihUc: {
     fontSize: 18,
     marginTop: 20
   },
-  MtTywSSj: {
+  iZCwRDzp: {
     fontSize: 18,
     marginTop: 20
   },
-  GyezklFA: {
+  hXHwWiWV: {
     fontSize: 18,
     marginTop: 20
   },
-  AnUdFtbf: {
+  ecaMnSZn: {
     color: "blue"
   }
 });
